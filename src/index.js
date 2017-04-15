fc20c7d16602158192afa68382a9f98def659615
Thierry Nowak <thinow@gmail.com>
2017-04-15 09:41:12 +0200

    redux: create first actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './redux/reducers/all';
 import App from './component/App';
 import './index.css';
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/redux/reducers/commons.js
@@ -0,0 +1,12 @@
+import * as type from '../actions/all';
+
+export default (state = {}, action = {}) => {
+  switch (action.type) {
+    
+    case type.APP_LOADING_SUCCESS:
+      return { loaded: true };
+    
+    default:
+      return state;
+  }
+}
