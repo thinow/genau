264d8ce779217cb2bb609c0a8285c643b7fa21d8
Thierry Nowak <thinow@gmail.com>
2017-04-23 15:31:10 +0200

    quick refactoring

--- src/component/loading/Loading.js
+++ src/component/loading/Loading.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 import { palette } from '../theme/theme';
+import Text from '../core/Text';
+
+const style = {
+  container: { textAlign: 'center' },
+  progress: { marginTop: '16vh' },
+  text: { fontSize: '7vw', color: palette.primary1Color, marginTop: '6vh' }
+};
 
 export default () => (
-  <div style={{textAlign: 'center'}}>
-    <CircularProgress style={{marginTop: '16vh'}} size={60} thickness={5} />
-    <p style={{ fontSize: '7vw', color: palette.primary1Color }}>Laden...</p>
+  <div style={style.container}>
+    <CircularProgress style={style.progress} size={60} thickness={5} />
+    <Text style={style.text}>Laden...</Text>
   </div>
 );
--- src/component/menu/Menu.js
+++ src/component/menu/Menu.js
@@ -2,25 +2,21 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Text from '../core/Text';
 
+const EXERCISES = ['Der, Die, Das', 'Plural', 'Perfekt', 'Präteritum', 'Alles!'];
+
 const style = {
   container: { textAlign: 'center' },
   title: { margin: '8vh 0 ' },
   button: { display: 'block', margin: '3vh 18vw' }
 };
 
-
-const EXERCISES = ['Der, Die, Das', 'Plural', 'Perfekt', 'Präteritum', 'Alles!'];
-
-export default () => {
-
-  return (
-    <div style={style.container}>
-      <Text style={style.title}>Was möchtest du üben ?</Text>
-      {
-        EXERCISES.map(exercise => {
-          return <RaisedButton key={exercise} label={exercise} style={style.button} primary={true} />;
-        })
-      }
-    </div>
-  );
-};
+export default () => (
+  <div style={style.container}>
+    <Text style={style.title}>Was möchtest du üben ?</Text>
+    {
+      EXERCISES.map(exercise => {
+        return <RaisedButton key={exercise} label={exercise} style={style.button} primary={true} />;
+      })
+    }
+  </div>
+);
