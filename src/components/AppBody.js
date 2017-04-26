26f418a71517731d7c7a7b3f6599ec210e670dad
Thierry Nowak <thinow@gmail.com>
2017-04-26 23:05:13 +0200

    display menu button

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/MenuItem';
 import Loading from './loading/Loading';
 import Menu from './menu/Menu';
 import Question from './question/Question';
@@ -7,6 +11,8 @@ import ErrorBar from './errorbar/ErrorBar';
 import { connect } from 'react-redux';
 import * as selectors from '../redux/selectors/selectors';
 import * as actions from '../redux/actions/all';
+
+const NO_ICON = <div></div>;
 
 const pages = [
   { name: 'loading', component: Loading },
@@ -28,6 +34,18 @@ export default connect(mapProps, mapCallbacks)(class extends Component {
     this.props.loadAppRequest();
   }
 
+  createMenuIcon = () => (
+    <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}>
+      <MenuItem primaryText="Menu" />
+    </IconMenu>
+  );
+
+  createAppBar = () => {
+    const icon = this.props.page === 'question' ? this.createMenuIcon() : NO_ICON;
+
+    return <AppBar title="Genau!" iconElementLeft={NO_ICON} iconElementRight={icon} />;
+  };
+
   createPageComponent = () => {
     const { component } = pages.find(({ name }) => name === this.props.page);
 
@@ -37,7 +55,7 @@ export default connect(mapProps, mapCallbacks)(class extends Component {
   render() {
     return (
       <div>
-        <AppBar title="Genau!" />
+        {this.createAppBar()}
         {this.createPageComponent()}
         <ErrorBar />
       </div>
