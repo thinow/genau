8cd2b80df78eced2af1acbeb92e2856923f31dd6
Thierry Nowak <thinow@gmail.com>
2017-04-28 23:13:22 +0200

    stub of question component

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 import * as selectors from '../../redux/selectors/selectors';
 import RaisedButton from 'material-ui/RaisedButton';
 import Text from '../core/Text';
+import { palette } from '../theme/theme';
 
 const style = {
-  container: {}
+  container: { textAlign: 'center' },
+  label: { marginTop: '16vh', fontWeight: 'bold', fontSize: '10vw' },
+  translation: { margin: '5vh 0', color: palette.disabledColor, fontSize: '4vw' },
+  button: { display: 'block', margin: '3vh 12vw' }
 };
 
 const mapProps = (state) => ({
@@ -14,9 +18,14 @@ const mapProps = (state) => ({
 
 const mapCallbacks = (dispatch) => ({});
 
+const createButton = ({ value }) => (
+  <RaisedButton key={value} label={value} style={style.button} />
+);
+
 export default connect(mapProps, mapCallbacks)(({ question }) => (
   <div style={style.container}>
-    <Text>{question.label}</Text>
-    {question.options.map(({ value }) => <RaisedButton key={value} label={value} />)}
+    <Text style={style.label}>{question.label}</Text>
+    <Text style={style.translation}>{question.translation}</Text>
+    {question.options.map(createButton)}
   </div>
 ));
