d2ed2a77db0ec41e6e79cd93eecf8d5ac0666716
Thierry Nowak <thinow@gmail.com>
2017-04-30 20:21:29 +0200

    bug report button

--- src/components/AppMenuIcon.js
+++ src/components/AppMenuIcon.js
@@ -3,10 +3,14 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import MoreVertical from 'material-ui/svg-icons/navigation/more-vert';
-import { palette } from './theme/theme'
+import FastRewind from 'material-ui/svg-icons/av/fast-rewind';
+import BugReport from 'material-ui/svg-icons/action/bug-report';
+import { palette } from './theme/theme';
 import { connect } from 'react-redux';
 import * as selectors from '../redux/selectors/selectors';
 import * as actions from '../redux/actions/all';
+
+const open = (link) => window.open(link, '_blank');
 
 const mapProps = (state) => ({
   page: selectors.getPage(state)
@@ -24,7 +28,8 @@ export default connect(mapProps, mapCallbacks)(class extends Component {
     switch (page) {
       case 'question':
         return [
-          { text: 'Menu', onClick: displayMenu }
+          { text: 'Menu', icon: <FastRewind />, onClick: displayMenu },
+          { text: 'Fehlermeldung', icon: <BugReport />, onClick: () => open('https://github.com/thinow/genau/issues/new') }
         ];
 
       default:
@@ -40,7 +45,7 @@ export default connect(mapProps, mapCallbacks)(class extends Component {
     } else {
       return (
         <IconMenu iconButtonElement={<IconButton><MoreVertical color={palette.canvasColor} /></IconButton>}>
-          {items.map(({ text, onClick }) => <MenuItem key={text} primaryText={text} onTouchTap={onClick} />)}
+          {items.map(({ text, icon, onClick }) => <MenuItem key={text} primaryText={text} leftIcon={icon} onTouchTap={onClick} />)}
         </IconMenu>
       );
     }
