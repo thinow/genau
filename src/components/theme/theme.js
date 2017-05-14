388c88bd76e1daff3204ddda146a377d6dfbce26
Thierry Nowak <thinow@gmail.com>
2017-05-14 23:31:49 +0200

    change colors

--- src/components/theme/theme.js
+++ src/components/theme/theme.js
@@ -1,9 +1,17 @@
 import chooseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import { green400 } from 'material-ui/styles/colors';
+import { blue500, red500, green400 } from 'material-ui/styles/colors';
 
-export const theme = chooseTheme;
+export const theme = {
+  ...chooseTheme,
+  palette: {
+    ...chooseTheme.palette,
+    primary1Color: blue500,
+    pickerHeaderColor: blue500,
+    accent1Color: red500
+  }
+};
 
 export const palette = {
-  ...chooseTheme.palette,
+  ...theme.palette,
   goodAnswer: green400
 };
