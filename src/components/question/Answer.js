49c5696cabf75ddf6eda49a27d010cd6769f4d5f
Thierry Nowak <thinow@gmail.com>
2017-05-16 07:59:21 +0200

    remove usage of class

--- src/components/question/Answer.js
+++ src/components/question/Answer.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import { palette } from '../theme/theme';
 import * as actions from '../../redux/actions/all';
-import * as selectors from '../../redux/selectors/selectors';
 
 const labelStyle = {
   textTransform: 'none',
@@ -16,22 +15,16 @@ const mapCallbacks = (dispath) => ({
   onClick: answer => dispath(actions.CHOOSE_ANSWER.create(answer))
 });
 
-export default connect(mapProps, mapCallbacks)(class extends Component {
+const create = (buttonProps) => {
+  return React.createElement(RaisedButton, { labelStyle, ...buttonProps });
+};
 
-  createButton = (buttonProps) => {
-    const { style } = this.props;
-    return React.createElement(RaisedButton, { style, labelStyle, ...buttonProps });
-  };
-
-  render() {
-    const { answer, onClick } = this.props;
-
-    if (!answer.chosen) {
-      return this.createButton({ label: answer.value, primary: true, onClick: () => onClick(answer) });
-    } else if (answer.correct) {
-      return this.createButton({ label: 'Genau!', backgroundColor: palette.goodAnswer });
-    } else {
-      return this.createButton({ label: 'Falsch', labelStyle: { ...labelStyle, color: palette.accent1Color } });
-    }
+export default connect(mapProps, mapCallbacks)(({ answer, style, onClick }) => {
+  if (!answer.chosen) {
+    return create({ label: answer.value, style, primary: true, onClick: () => onClick(answer) });
+  } else if (answer.correct) {
+    return create({ label: 'Genau!', style, backgroundColor: palette.goodAnswer });
+  } else {
+    return create({ label: 'Falsch', style, labelStyle: { ...labelStyle, color: palette.accent1Color } });
   }
 });
